cd5d593f3c6febab4efdab2d78eeadb9be7d09d4
kristijan-pajtasev <kristijan.pajtasev@hotmail.com>
2018-10-29 19:50:23 +0000

    assignment 1 fix

--- src/Assignment1.java
+++ src/Assignment1.java
@@ -24,44 +24,50 @@ public class Assignment1 {
 
     static void runAssignment1(int diceThrows) {
         int[] diceResults = new int[diceThrows];
-        Dice t = null;
-        for(int i = 0; i < 4; i++) {
-            t = new Dice(diceThrows / 4, diceResults, i * diceThrows / 4);
-            t.run();
+        Dice[] dices = new Dice[4];
+        for (int i = 0; i < 4; i++) {
+            Dice dice = new Dice(diceThrows / 4, diceResults, i * diceThrows / 4);
+            dices[i] = dice;
+            dice.start();
         }
         try {
-            t.join();
+            for (int i = 0; i < 4; i++) { dices[i].join(); }
             int[] results = new int[6];
-            for(int diceThrow : diceResults) {
+            for (int diceThrow : diceResults) {
                 results[diceThrow - 1]++;
             }
-            for(int i = 0; i < 6; i ++) System.out.println("Number " + (i + 1) + " appears " + results[i] + " times");
+            for (int i = 0; i < 6; i++) System.out.println("Number " + (i + 1) + " appears " + results[i] + " times");
 
-        } catch (InterruptedException e) { e.printStackTrace(); }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 
     // task 2
     static void runAssignment2(int N) {
         int data[] = new int[N];
         //assume occurrence of zero equally likely for all numbers generated
-        for(int j = 0; j < N; j++) data[j] = (int)(Math.random()*N);
+        for (int j = 0; j < N; j++) data[j] = (int) (Math.random() * N);
 
-        LeftmostZero t = null;
         int[] result = {-1, -1, -1, -1};
-        for(int i = 0; i < 4; i++) {
-            t = new LeftmostZero(i, data, N / 4, N / 4 * i, result);
-            t.run();
+        LeftmostZero[] results = new LeftmostZero[4];
+        for (int i = 0; i < 4; i++) {
+            LeftmostZero t = new LeftmostZero(i, data, N / 4, N / 4 * i, result);
+            results[i] = t;
+            t.start();
         }
         try {
-            t.join();
-            for(int i = 0; i < 4; i++) {
-                if(result[i] >= 0) {
+            for (int i = 0; i < 4; i++) { results[i].join(); }
+            for (int i = 0; i < 4; i++) {
+                if (result[i] >= 0) {
                     System.out.println(result[i]);
                     return;
                 }
             }
             System.out.println("No zero");
-        } catch (InterruptedException e) { e.printStackTrace(); }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
 
     }
 }
@@ -71,18 +77,41 @@ public class Assignment1 {
 
 // Assignment 01 class
 class Dice extends Thread {
-    public Dice(int n, int[] results, int offset) {
-        for(int i = 0; i < n; i++) {
-            results[offset + i] = (int)Math.floor(Math.random() * 6) + 1;
+    private final int offset;
+    private final int[] results;
+    private final int n;
+
+    Dice(int n, int[] results, int offset) {
+        this.n = n;
+        this.results = results;
+        this.offset = offset;
+    }
+
+    @Override
+    public void run() {
+        for (int i = 0; i < n; i++) {
+            results[offset + i] = (int) Math.floor(Math.random() * 6) + 1;
         }
     }
 }
 
 // Assignment 02 class
 class LeftmostZero extends Thread {
-    public LeftmostZero(int index, int[] arr, int length, int offset, int[] result) {
-        for(int i = 0; i < length; i++) {
-            if(arr[offset + i] == 0) {
+    private int index, length, offset;
+    private int[] result, arr;
+
+    LeftmostZero(int index, int[] arr, int length, int offset, int[] result) {
+        this.index = index;
+        this.length = length;
+        this.offset = offset;
+        this.arr = arr;
+        this.result = result;
+    }
+
+    @Override
+    public void run() {
+        for (int i = 0; i < length; i++) {
+            if (arr[offset + i] == 0) {
                 result[index] = offset + i;
                 break;
             }
