d2a52118f4c864eeec2ce97c64b90c0a614c4d96
kristijan-pajtasev <kristijan.pajtasev@hotmail.com>
2018-11-10 14:01:54 +0000

    Assignment5 init

---
+++ src/Assignment5.java
@@ -0,0 +1,19 @@
+/**
+ * Assignment5.java
+ * Student Name: Kristijan Pajtasev
+ * Student Number: 2920266
+ */
+
+public class Assignment5 {
+    public static void main(String args[]) {
+    }
+}
+
+final class Point{
+    private final double x, y;
+    public Point(double x0, double y0){x = x0; y = y0;}
+    public double x(){return x;}
+    public double y(){return y;}
+    public String toString(){return "("+x+","+y+")";}
+}
+
