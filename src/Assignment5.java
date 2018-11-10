f76e850c312c9e921697eb6960f650993a09ab60
kristijan-pajtasev <kristijan.pajtasev@hotmail.com>
2018-11-10 14:31:09 +0000

    Assignment5 q1

--- src/Assignment5.java
+++ src/Assignment5.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 /**
  * Assignment5.java
  * Student Name: Kristijan Pajtasev
@@ -9,6 +11,32 @@ public class Assignment5 {
     }
 }
 
+class CollectionPoint {
+    private ArrayList<Point> points = new ArrayList<>();
+
+    public void add(Point p) {
+    }
+
+    public boolean search(Point p) {
+        return true;
+    }
+
+    public ArrayList<Point> getAllX(int x) {
+        return new ArrayList<>();
+    }
+
+    public void replace(Point p) {
+
+    }
+
+    @Override
+    public String toString() {
+        return "CollectionPoint{" +
+                "points=" + points +
+                '}';
+    }
+}
+
 final class Point{
     private final double x, y;
     public Point(double x0, double y0){x = x0; y = y0;}
@@ -16,4 +44,3 @@ final class Point{
     public double y(){return y;}
     public String toString(){return "("+x+","+y+")";}
 }
-
