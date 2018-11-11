0dc4a42bc92d186257eb26131d90e0acc5a74232
kristijan-pajtasev <kristijan.pajtasev@hotmail.com>
2018-11-11 02:19:23 +0000

    Assignment5 q2

--- src/Assignment5.java
+++ src/Assignment5.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.Exchanger;
 import java.util.concurrent.Semaphore;
 
 /**
@@ -22,8 +23,7 @@ public class Assignment5 {
 
         //Question 3
         //==============================================
-
-
+        questionThreeTest();
         //===============================================
     }
 
@@ -83,6 +83,14 @@ public class Assignment5 {
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
+    }
+
+    static void questionThreeTest() {
+        Exchanger<ArrayList<Integer>> exchanger = new Exchanger<>();
+        ExchangerProducer producer = new ExchangerProducer(exchanger);
+        ExchangerConsumer consumer = new ExchangerConsumer(exchanger);
+        producer.start();
+        consumer.start();
     }
 }
 
@@ -213,5 +221,58 @@ class QuestionTwoThread extends Thread {
 
 
 //Q3 ===========================================================
+
+class ExchangerProducer extends Thread {
+    private Exchanger<ArrayList<Integer>> exchanger;
+    private ArrayList<Integer> buffer = new ArrayList<Integer>();
+
+    public ExchangerProducer(Exchanger<ArrayList<Integer>> exchanger) {
+        this.exchanger = exchanger;
+    }
+
+    public void run() {
+        while (true) {
+            try {
+                System.out.println("Producer.");
+                Thread.sleep(1000);
+                fillBuffer();
+                System.out.println("Producer has produced and waiting:" + buffer);
+                buffer = exchanger.exchange(buffer);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void fillBuffer() {
+        for (int i = 0; i <= 3; i++) {
+            buffer.add(i);
+        }
+    }
+}
+
+class ExchangerConsumer extends Thread {
+    private Exchanger<ArrayList<Integer>> exchanger;
+    private ArrayList<Integer> buffer = new ArrayList<Integer>();
+
+    public ExchangerConsumer(Exchanger<ArrayList<Integer>> exchanger) {
+        this.exchanger = exchanger;
+    }
+
+    public void run() {
+        while (true) {
+            try {
+                System.out.println("Consumer.");
+                buffer = exchanger.exchange(buffer);
+                System.out.println("Consumer has received:" + buffer);
+                Thread.sleep(1000);
+                System.out.println("eating:" + buffer);
+                buffer.clear();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
 
 //End Q3 =======================================================
